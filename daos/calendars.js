57b7b39a64fd2ac7207adcf3cdbc903245fe3956
Nandini Rao [nandinirao] <nandinirao@costco.com>
2023-04-23 18:52:57 -0700

    calendar daos/routes passing tests

--- daos/calendars.js
+++ daos/calendars.js
@@ -6,6 +6,10 @@ module.exports.create = async (name) => {
   return await Calendars.create({ name });
 };
 
+module.exports.getAll = async () => {
+  return await Calendars.find({});
+};
+
 module.exports.getById = async (id) => {
   try {
     const calendar = await Calendars.findOne({ _id: id }).lean();
@@ -18,6 +22,15 @@ module.exports.getById = async (id) => {
 module.exports.updateById = async (id, newData) => {
   try {
     const calendar = await Calendars.findOneAndUpdate({ _id: id }, newData, { new: true }).lean();
+    return calendar;
+  } catch (e) {
+    return null;
+  }
+};
+
+module.exports.removeById = async (id) => {
+  try {
+    const calendar = await Calendars.deleteOne({ _id: id }).lean();
     return calendar;
   } catch (e) {
     return null;
--- routes/calendars.js
+++ routes/calendars.js
@@ -4,6 +4,28 @@ const CalendarDAO = require('../daos/calendars');
 
 const router = Router();
 
+router.post("/", async (req, res, next) => {
+  const calendar = req.body;
+  if (!calendar || JSON.stringify(calendar) === '{}' || !calendar.name) {
+    res.status(400).send('calendar and calendar name is required');
+  } else {
+    const savedCalendar = await CalendarDAO.create(calendar.name);
+    res.json(savedCalendar);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  const calendarId = req.params.id;
+  const calendar = req.body;
+  if (!calendar || JSON.stringify(calendar) === '{}'  || !calendar.name) {
+    res.status(400).send('calendar and calendar name is required');
+  } else {
+    const updatedCalendar = await CalendarDAO.updateById(calendarId, calendar);
+    updatedCalendar ? res.json(updatedCalendar) : res.status(404).send(`calendar id ${calendarId} not found.`)
+  }
+});
+
+
 router.get("/", async (req, res, next) => {
   try {
     const calendars = await CalendarDAO.getAll();
